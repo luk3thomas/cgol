f1d794b13ed22957e452f37e48c72b8cc4be04c5
Luke Thomas <luk3thomas@gmail.com>
2013-12-31 01:05:27 -0600

    update draw

--- lib/screen.rb
+++ lib/screen.rb
@@ -5,7 +5,12 @@ class Screen
 
   def draw!
     clear
-    puts @board.cells.map{|row| row.map{|cell| cell.alive ? '*' : ' ' }.join(' ') }.join("\n")
+    map = @board.cells.map{|row| row.map{|cell| cell.alive ? '+' : ' ' }.join(' ')}.join("\n")
+    border = '-' * (@board.columns * 2 - 1)
+    border = ['+', border, '+'].join('')
+    map.gsub!(/^|$/, '|')
+    map = [border, map, border].join("\n")
+    puts map
     puts "Population: #{@board.cells.flatten.select{|n| n.alive}.size}"
   end
 
