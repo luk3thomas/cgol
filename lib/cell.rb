0e1fce9a778d4482266094ea73185348ac830991
Luke Thomas <luk3thomas@gmail.com>
2013-12-30 20:39:14 -0600

    fix initial state assignment

--- lib/cell.rb
+++ lib/cell.rb
@@ -2,6 +2,23 @@ class Cell
   attr_accessor :alive
 
   def initialize alive = nil
-    @alive = alive || false
+    @alive = determine_state(alive)
+  end
+
+  private
+  def determine_state input
+    case true
+    # Numbers
+    when input.is_a?(Integer)
+      !input.zero?
+    # Strings
+    when input.is_a?(String)
+      !input.size.zero?
+    # Boolean
+    when !!input == input
+      input
+    else
+      false
+    end
   end
 end
--- spec/lib/cell_spec.rb
+++ spec/lib/cell_spec.rb
@@ -10,5 +10,15 @@ describe Cell do
     it "is false by default" do
       @cell.alive.should eq(false)
     end
+    it "can be initialized as true" do
+      [true, 1, "some string", 2039].each do |state|
+        Cell.new(state).alive.should eq(true)
+      end
+    end
+    it "can be initialized as false" do
+      [false, 0, ""].each do |state|
+        Cell.new(state).alive.should eq(false)
+      end
+    end
   end
 end
