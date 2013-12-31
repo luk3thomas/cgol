b58adf8510ed675bcdf3917dbaf704d90080d05b
Luke Thomas <luk3thomas@gmail.com>
2013-12-30 18:57:01 -0600

    populate board with cells on init

--- lib/board.rb
+++ lib/board.rb
@@ -1,5 +1,7 @@
+require 'cell'
+
 class Board
-  attr_accessor :size
+  attr_accessor :size, :cells
 
   def initialize args = {}
     defaults = {
@@ -8,5 +10,26 @@ class Board
     defaults.merge(args).each do |k, v|
       instance_variable_set( "@#{k}", v )
     end
+    populate
   end
+
+  def rows
+    @size.first
+  end
+
+  def columns
+    @size.last
+  end
+
+  private
+    def populate
+      @cells = []
+      rows.times do
+        _row = []
+        columns.times do
+          _row << Cell.new
+        end
+        @cells << _row
+      end
+    end
 end
--- spec/lib/board_spec.rb
+++ spec/lib/board_spec.rb
@@ -2,9 +2,14 @@ require 'spec_helper'
 require 'board'
 
 describe Board do
-  context "Default variables" do
-    before(:each) { @board = Board.new }
+  before(:each) { @board = Board.new }
 
+  context "Methods" do
+    it { @board.rows.should eq(@board.size.first) }
+    it { @board.columns.should eq(@board.size.last) }
+  end
+
+  context "Default variables" do
     context "size" do
       it "should have a default size array" do
         @board.size.is_a?(Array).should eq(true)
@@ -25,6 +30,17 @@ describe Board do
         @board2 = Board.new( size: custom_dimensions )
         @board2.size.should eq(custom_dimensions)
       end
+    end
+  end
+  context "Cells" do
+    it "should be an array of arrays" do
+      @board.cells.is_a?(Array).should eq(true)
+    end
+    it "the cell rows should equal the board rows" do
+      @board.cells.count.should eq(@board.rows)
+    end
+    it "the cell columns should equal the board columns" do
+      @board.cells.first.count.should eq(@board.columns)
     end
   end
 end
