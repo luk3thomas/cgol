209166aca7d810a37a4b4810c53f0b0d3442499c
Luke Thomas <luk3thomas@gmail.com>
2013-12-31 00:14:17 -0600

    add cycle method for atomaton

--- lib/automaton.rb
+++ lib/automaton.rb
@@ -4,18 +4,33 @@ class Automaton
   end
 
   def cycle!
-    @board.cells.each_with_index do |row, i|
-      @board.cells[i].each_with_index do |cell, j|
-        @board.cells[i][j].alive = alive?([i, j]) 
+    _cells = @board.cells.each_with_index.map do |row, i|
+      row.each_with_index.map do |cell, j|
+        alive?(cell, [i, j])
+      end
+    end
+
+    _cells.each_with_index do |row, i|
+      row.each_with_index do |cell, j|
+        @board.cells[i][j].alive = _cells[i][j]
       end
     end
   end
 
-  def alive? coords
-    sustained?(coords) || lush?(coords)
+  def alive? cell, coords
+    neighbors = select_neighbors(coords)
+    if cell.alive
+      sustained?(neighbors) || !( lonely?(neighbors) || crowded?(neighbors) )
+    else
+      lush?(neighbors)
+    end
   end
 
-  def neighbors coords
+  def select_neighbors coords
+    find_neighbors(coords).map{|n| @board.cells[n.first][n.last]}.flatten.uniq
+  end
+
+  def find_neighbors coords
     row, column = coords
     arr = [
       [row - 1, column - 1] ,
@@ -30,12 +45,26 @@ class Automaton
     within_bounds(coords) ? arr.select {|c| within_bounds(c) } : []
   end
 
+  def lonely? neighbors
+    occupied(neighbors).between?(0, 1)
+  end
+
+  def crowded? neighbors
+    occupied(neighbors).between?(4, 8)
+  end
+
+  def sustained? neighbors
+    occupied(neighbors).between?(2, 3)
+  end
+
+  def lush? neighbors
+    occupied(neighbors) == 3
+  end
+
   private
-    def sustained? coords
-      row, column = coords
-    end
-    def lush? coords
-      row, column = coords
+
+    def occupied neighbors
+      neighbors.map{|n| n.alive || nil }.compact.size
     end
 
     def within_bounds coords
