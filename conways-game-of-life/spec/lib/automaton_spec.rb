71540b9ba7b6f0131eda0437d785e0e2f0f5bf13
Luke Thomas <luk3thomas@gmail.com>
2014-01-01 13:09:59 -0600

    test for infinite canvas

--- conways-game-of-life/spec/lib/automaton_spec.rb
+++ conways-game-of-life/spec/lib/automaton_spec.rb
@@ -3,73 +3,88 @@ require 'automaton'
 require 'board'
 
 blinker = [
-  [0, 0, 0],
-  [1, 1, 1],
-  [0, 0, 0],
+  [0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0],
+  [0, 1, 1, 1, 0],
+  [0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0],
 ]
 
 toad = [
-  [0, 0, 0, 0],
-  [0, 1, 1, 1],
-  [1, 1, 1, 0],
-  [0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0],
+  [0, 0, 1, 1, 1, 0],
+  [0, 1, 1, 1, 0, 0],
+  [0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0],
 ]
 
+board_blinker = Board.new([5,5], blinker)
+board_toad = Board.new([6,6], toad)
+
 describe Automaton do
+
   before(:each) do
-    @board = Board.new [3,3], blinker 
-    @big_board = Board.new([5,5], toad)
-    @board2 = Board.new [3,3], [[0, 1, 0], [0, 1, 0], [0, 1,0]]
+    @board = board_blinker
+    @big_board = board_toad
     @automaton = Automaton.new @board
   end
 
   #    Blinker: @board
-  #    3 x 3
-  #
-  #    +-----------+     +-----------+
-  #    |   |   |   |     |   | X |   |
-  #    +-----------+     +-----------+
-  #    | X | X | X |  => |   | X |   |
-  #    +-----------+     +-----------+
-  #    |   |   |   |     |   | X |   |
-  #    +-----------+     +-----------+
+  #    5 x 5
+  #           
+  #    +-------------------+     +-------------------+
+  #    |   |   |   |   |   |     |   |   |   |   |   |
+  #    +-------------------+     +-------------------+
+  #    |   |   |   |   |   |     |   |   | X |   |   |
+  #    +-------------------+     +-------------------+
+  #    |   | X | X | X |   |  => |   |   | X |   |   |
+  #    +-------------------+     +-------------------+
+  #    |   |   |   |   |   |     |   |   | X |   |   |
+  #    +-------------------+     +-------------------+
+  #    |   |   |   |   |   |     |   |   |   |   |   |
+  #    +-------------------+     +-------------------+
 
   #    Toad: @big_board
-  #    4 x 4
+  #    6 x 6
   #
-  #    +---------------+     +---------------+
-  #    |   |   |   |   |     |   |   | X |   |
-  #    +---------------+     +---------------+
-  #    |   | X | X | X |  => | X |   |   | X |
-  #    +---------------+     +---------------+
-  #    | X | X | X |   |     | X |   |   | X |
-  #    +---------------+     +---------------+
-  #    |   |   |   |   |     |   | X |   |   |
-  #    +---------------+     +---------------+
+  #    +-----------------------+     +-----------------------+
+  #    |   |   |   |   |   |   |     |   |   |   |   |   |   |
+  #    +-----------------------+     +-----------------------+
+  #    |   |   |   |   |   |   |     |   |   |   | X |   |   |
+  #    +-------------------+---+     +---+-------------------+
+  #    |   |   | X | X | X |   |  => |   | X |   |   | X |   |
+  #    +-------------------+---+     +---+-------------------+
+  #    |   | X | X | X |   |   |     |   | X |   |   | X |   |
+  #    +-------------------+---+     +---+-------------------+
+  #    |   |   |   |   |   |   |     |   |   | X |   |   |   |
+  #    +-------------------+---+     +---+-------------------+
+  #    |   |   |   |   |   |   |     |   |   |   |   |   |   |
+  #    +-----------------------+     +-----------------------+
 
   context "identifies state" do
     it "lonely" do
-      neighbors = @automaton.select_neighbors([1,2])
-      @board.cells[1][2].alive.should eq(true)
+      neighbors = @automaton.select_neighbors([2,1])
+      @board.cells[2][1].alive.should eq(true)
       @automaton.lonely?(neighbors).should eq(true)
     end
 
     it "lush" do
-      neighbors = @automaton.select_neighbors([0,1])
-      @board.cells[0][1].alive.should eq(false)
+      neighbors = @automaton.select_neighbors([1,2])
+      @board.cells[1][2].alive.should eq(false)
       @automaton.lush?(neighbors).should eq(true)
     end
 
     it "sustain" do
-      neighbors = @automaton.select_neighbors([1,1])
-      @board.cells[1][1].alive.should eq(true)
+      neighbors = @automaton.select_neighbors([2,2])
+      @board.cells[2][2].alive.should eq(true)
       @automaton.sustained?(neighbors).should eq(true)
     end
 
     it "crowded" do
       @automaton = Automaton.new @big_board
-      neighbors = @automaton.select_neighbors([1,1])
-      @board.cells[1][1].alive.should eq(true)
+      neighbors = @automaton.select_neighbors([2,2])
+      @board.cells[2][2].alive.should eq(true)
       @automaton.crowded?(neighbors).should eq(true)
     end
   end
@@ -77,15 +92,17 @@ describe Automaton do
 
   context "correctly ocillates blinker" do
     before(:all) do
-      @b = Board.new([3, 3], blinker)
+      @b = board_blinker
       @automaton = Automaton.new( @b )
       @automaton.cycle!
     end
 
     [
-      [ false, true, false ] ,
-      [ false, true, false ] ,
-      [ false, true, false ] ,
+      [ false , false , false , false , false ] , 
+      [ false , false , true  , false , false ] , 
+      [ false , false , true  , false , false ] , 
+      [ false , false , true  , false , false ] , 
+      [ false , false , false , false , false ] , 
     ].each_with_index do |row, i|
       row.each_with_index do |col, j|
         it "[#{i}, #{j}] is #{col}" do
@@ -97,16 +114,18 @@ describe Automaton do
 
   context "correctly hops the toad" do
     before(:all) do
-      @b = Board.new([4, 4], toad)
+      @b = board_toad
       @automaton = Automaton.new( @b )
       @automaton.cycle!
     end
 
     [
-      [ false , false , true  , false ] , 
-      [ true  , false , false , true ]  , 
-      [ true  , false , false , true ]  , 
-      [ false , true  , false , false ] , 
+      [ false , false , false , false , false , false ] , 
+      [ false , false , false , true  , false , false ] , 
+      [ false , true  , false , false , true  , false ] , 
+      [ false , true  , false , false , true  , false ] , 
+      [ false , false , true  , false , false , false ] , 
+      [ false , false , false , false , false , false ] , 
     ].each_with_index do |row, i|
       row.each_with_index do |col, j|
         it "[#{i}, #{j}] is #{col}" do
@@ -118,29 +137,13 @@ describe Automaton do
 
   context "Selecting neighbors" do
     data = [
-      [ [0,0], [[0,1], [1,1], [1,0]] ],
-      [ [0,1], [[0,0], [0,2], [1,0], [1,1], [1,2]] ],
-      [ [0,2], [[0,1], [1,1], [1,2]] ],
-      [ [1,0], [[0,0], [0,1], [1,1], [2,1], [2,0]] ],
       [ [1,1], [[0,0], [0,1], [0,2], [1,0], [1,2], [2,0], [2,1], [2,2]] ],
-      [ [1,2], [[0,1], [0,2], [1,1], [2,1], [2,2]] ],
-      [ [2,0], [[1,0], [1,1], [2,1]] ],
-      [ [2,1], [[1,0], [1,1], [1,2], [2,0], [2,2]] ],
-      [ [2,2], [[1,1], [1,2], [2,1]] ],
 
       # Out of bounds
-      [ [-1,0], [[1,0], [1,1], [2,1]] ],
-      [ [-1,1], [[1,0], [1,1], [1,2], [2,0], [2,2]] ],
-      [ [-1,2], [[1,1], [1,2], [2,1]] ],
-      [ [3,0], [[0,1], [1,1], [1,0]] ],
-      [ [3,1], [[0,0], [0,2], [1,0], [1,1], [1,2]] ],
-      [ [3,2], [[0,1], [1,1], [1,2]] ],
-      [ [0,-1], [[0,1], [1,1], [1,2]] ],
-      [ [1,-1], [[0,1], [0,2], [1,1], [2,1], [2,2]] ],
-      [ [2,-1], [[1,1], [1,2], [2,1]] ],
-      [ [0,3], [[0,1], [1,1], [1,0]] ],
-      [ [1,3], [[0,0], [0,1], [1,1], [2,1], [2,0]] ],
-      [ [2,3], [[1,0], [1,1], [2,1]] ],
+      [ [0,0], [[0,1], [1,1], [1,0], [1,4], [0,4], [4,4], [4,0], [4,1]] ],
+      [ [0,4], [[4,3], [4,4], [4,0], [0,0], [1,0], [1,4], [1,3], [0,3]] ],
+      [ [4,0], [[3,0], [3,1], [4,1], [0,0], [4,4], [0,1], [3,4], [0,4]] ],
+      [ [4,4], [[3,4], [3,3], [4,3], [0,0], [0,4], [4,0], [3,0], [0,3]] ],
     ]
     data.each do |d|
       it "selects #{d.last.size} neighbors for #{d.first}" do
